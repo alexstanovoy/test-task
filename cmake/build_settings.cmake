6d6633695ecc85c3eb9c0286f0e40c7dbc6d6c20
Alexander Stanovoy <alex.stanovoy@gmail.com>
2021-06-14 01:17:29 +0300

    Travis lnk options fix

--- cmake/build_settings.cmake
+++ cmake/build_settings.cmake
@@ -1,5 +1,5 @@
-set(SOURCES             "src")
+set(SOURCES "src")
 add_compile_options("-Wall" "-Wextra" "-pedantic" "-Werror" "-O0" "-g" "--coverage")
-add_link_options("-lgcov" "--coverage")
+#add_link_options("-lgcov" "--coverage")  # No add_link_options in Xenial! Sad
 
 include_directories(${SOURCES})
