16c3991f2803d601484eb4e4ed96adb7dff96c1b
Alexander Stanovoy <alex.stanovoy@gmail.com>
2021-06-14 01:28:47 +0300

    Ok, I'll just use Ubuntu 20.04...

--- cmake/build_settings.cmake
+++ cmake/build_settings.cmake
@@ -1,5 +1,5 @@
 set(SOURCES "src")
-add_compile_options("-Wall" "-Wextra" "-pedantic" "-Werror" "-O0" "-g" "--coverage")
-#add_link_options("-lgcov" "--coverage")  # No add_link_options in Xenial! Sad
+add_compile_options("-Wall" "-Wextra" "-pedantic" "-Werror" "-O0" "-g" "--coverage" "-lgcov" "--coverage")
+add_link_options("-lgcov" "--coverage")
 
 include_directories(${SOURCES})
